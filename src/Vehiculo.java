8c13dc8f3770558197980347b25debdc86052f99
Radfer10 <rauldemi2000@gmail.com>
2024-11-02 21:52:30 +0100

    Funcionalidad tlf1 ok

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,11 +1,14 @@
 
 public class Vehiculo 
 {
+	public static void arrancar()
+	{
+		System.out.println("Brrrrr....acaba de arrancar!");	
+	}
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Brrrrr....acaba de arrancar");
-
+		arrancar();
 	}
 
 }
